570dd5a651d18dcaf204d6a6dc8c0a5f4d4fd167
JoseAlejandroLopezJaimes <48494286+JoseAlejandroLopezJaimes@users.noreply.github.com>
2021-02-19 12:28:06 -0600

    Add files via upload

---
+++ movie_rating_app/config/Config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    DB : 'mongodb://localhost/movie_rating_app',
+    SECRET: 'movieratingappsecretkey',
+    FACEBOOK_APP_ID: '475274390150948',
+    FACEBOOK_APP_SECRET: '6f683e90611c17f768203daffd6f5880',
+    GOOGLE_APP_ID: '917232262012-vsn0pjmvkuqa16h046t9to8fqrlt4503.apps.googleusercontent.com',
+    GOOGLE_APP_SECRET: '8zhKUzg5E-AgmGnEn39fHzKR',
+    LINKEDIN_APP_ID: '78iayluhnbijaz',
+    LINKEDIN_APP_SECRET: 'k1xd6lmSkqHIVm98'
+}
